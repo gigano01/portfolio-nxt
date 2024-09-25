6ca3c430b21efb0435ee21b4d691c509bb3493ff
gigano01 <noahvanleemput@hotmail.com>
2024-09-25 23:24:47 +0200

    added insight

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
-import "normalize.css"
+import "normalize.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
+            <SpeedInsights />
             <Head>
                 <title>Noahs Portfolio</title>
             </Head>
